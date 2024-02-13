7c6cfd28af214fea8b4e13988873d18fa0247f58
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-13 15:55:14 +0530

    feat: init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bitwise
+
+This project is setup with `nx` a new tool found in the wild.
+
+# Setup
+
+## Prerequisites
+
+- Have `node` installed. (Any version >= 18 should works)
---
+++ schemas/src/lib/schemas.ts
@@ -0,0 +1,11 @@
+import { InferSchemaType, model, Schema } from "mongoose"
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+})
+
+export type User = InferSchemaType<typeof userSchema>
+
+export const UserModel = model("User", userSchema)
