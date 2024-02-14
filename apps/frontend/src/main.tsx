c931b43d2c4df159ede15754f6fa589d0f211854
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-14 11:06:26 +0530

    feat: add root route and temporary test route

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,9 +3,20 @@ import '@mantine/core/styles.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
-import App from './app/app'
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './features/app'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/test',
+		element: <div>tested</div>,
+	},
+])
 
 const queryClient = new QueryClient()
 
@@ -13,7 +24,7 @@ root.render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<MantineProvider>
-				<App />
+				<RouterProvider router={router} />
 			</MantineProvider>
 		</QueryClientProvider>
 	</StrictMode>,
