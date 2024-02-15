8bdb627195c2e448154c4ff1615c5018356810a0
majimearun <arunachala0619@gmail.com>
2024-02-16 00:52:45 +0530

    refactor: shift wallet inside user (embedded)

--- schemas/src/lib/schemas.ts
+++ schemas/src/lib/schemas.ts
@@ -15,33 +15,21 @@ const userSchema = new Schema({
 		required: true,
 	},
 	wallet: {
-		type: Schema.Types.ObjectId,
-		ref: 'Wallet',
-		required: true,
+		balance: {
+			type: Number,
+			required: true,
+			min: 0,
+		},
+		tags: {
+			type: [String],
+			default: [],
+		},
 	},
 })
 
 export type NewUser = InferSchemaType<typeof userSchema>
 export type User = HydratedDocument<NewUser>
 export const UserModel = model('User', userSchema)
-
-const walletSchema = new Schema({
-	balance: {
-		type: Number,
-		required: true,
-		min: 0,
-	},
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-
-})
-
-export type NewWallet = InferSchemaType<typeof walletSchema>
-export type Wallet = HydratedDocument<NewWallet>
-export const WalletModel = model('Wallet', walletSchema)
 
 const transactionSchema = new Schema({
 	amount: {
