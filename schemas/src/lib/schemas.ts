6ff445ed4d7870bafa3441f5f0507bc7c7c30b5c
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-02-15 13:47:31 +0530

    refactor(schemas): edit user schema

--- schemas/src/lib/schemas.ts
+++ schemas/src/lib/schemas.ts
@@ -1,10 +1,20 @@
-import { InferSchemaType, model, Schema } from 'mongoose'
+import { HydratedDocument, InferSchemaType, model, Schema } from 'mongoose'
 
 const userSchema = new Schema({
-	name: String,
-	email: String,
-	password: String,
+	name: {
+		type: String,
+		required: [true, 'User must have a name'],
+	},
+	email: {
+		type: String,
+		required: [true, 'User must have an email'],
+		unique: [true, 'This email has already been used'],
+	},
+	password: {
+		type: String,
+		required: [true, 'User must have a password'],
+	},
 })
 
-export type User = InferSchemaType<typeof userSchema>
+export type User = HydratedDocument<InferSchemaType<typeof userSchema>>
 export const UserModel = model('User', userSchema)
