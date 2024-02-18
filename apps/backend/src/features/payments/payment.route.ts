72f700c208bd803d4467445a083dc3e6103455c5
majimearun <arunachala0619@gmail.com>
2024-02-18 17:27:16 +0530

    feat: add payment router

---
+++ apps/backend/src/features/payments/payment.route.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import {
+	transact,
+    validateTransaction
+} from './payment.handler'
+
+export const paymentRouter = express.Router()
+paymentRouter.post('/', validateTransaction, transact)
