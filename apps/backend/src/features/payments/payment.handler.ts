9651b9058d02cae879944b653338fd0f9ead1436
majimearun <arunachala0619@gmail.com>
2024-02-18 17:29:31 +0530

    style: format code files with prettier

--- apps/backend/src/features/payments/payment.handler.ts
+++ apps/backend/src/features/payments/payment.handler.ts
@@ -8,46 +8,51 @@ import { BadRequest } from '../../errors/CustomErrors'
 
 export const validateTransaction = [
 	body('senderId').trim().notEmpty().withMessage('From account is required'),
-    body('receiverId').trim().notEmpty().withMessage('To account is required'),
-    body('amount').isNumeric().toInt().withMessage('Amount must be a number'),
+	body('receiverId').trim().notEmpty().withMessage('To account is required'),
+	body('amount').isNumeric().toInt().withMessage('Amount must be a number'),
 ]
 
 export const transact: RequestHandler = async (req, res, next) => {
-    try{
-        const { senderId, receiverId, amount } = validateRequest<Transaction>(req)
-        const sender = await prisma.user.findUnique({ where: { username: senderId } })
-        if(!sender){
-            throw new BadRequest('Sender not found')
-        }
-        const receiver = await prisma.user.findUnique({ where: { username: receiverId } })
-        if(!receiver){
-            throw new BadRequest('Receiver not found')
-        }
-        if(sender.balance < amount){
-            throw new BadRequest('Insufficient balance')
-        }
-        await prisma.$transaction([
-            prisma.user.update({
-                where: { username: senderId },
-                data: { balance: { decrement: amount } }
-            }),
-            prisma.user.update({
-                where: { username: receiverId },
-                data: { balance: { increment: amount } }
-            }),
-            prisma.transaction.create({
-                data: {
-                    senderId,
-                    receiverId,
-                    amount,
-                    status: true,
-                    createdAt: new Date()
-                }
-            })
-        ])
-        return res.status(StatusCodes.CREATED).json({ message: 'Transaction successful', errors: []})
-
-    }catch(err){
-        next(err)
-    }
+	try {
+		const { senderId, receiverId, amount } = validateRequest<Transaction>(req)
+		const sender = await prisma.user.findUnique({
+			where: { username: senderId },
+		})
+		if (!sender) {
+			throw new BadRequest('Sender not found')
+		}
+		const receiver = await prisma.user.findUnique({
+			where: { username: receiverId },
+		})
+		if (!receiver) {
+			throw new BadRequest('Receiver not found')
+		}
+		if (sender.balance < amount) {
+			throw new BadRequest('Insufficient balance')
+		}
+		await prisma.$transaction([
+			prisma.user.update({
+				where: { username: senderId },
+				data: { balance: { decrement: amount } },
+			}),
+			prisma.user.update({
+				where: { username: receiverId },
+				data: { balance: { increment: amount } },
+			}),
+			prisma.transaction.create({
+				data: {
+					senderId,
+					receiverId,
+					amount,
+					status: true,
+					createdAt: new Date(),
+				},
+			}),
+		])
+		return res
+			.status(StatusCodes.CREATED)
+			.json({ message: 'Transaction successful', errors: [] })
+	} catch (err) {
+		next(err)
+	}
 }
--- apps/backend/src/features/payments/payment.route.ts
+++ apps/backend/src/features/payments/payment.route.ts
@@ -1,8 +1,5 @@
 import express from 'express'
-import {
-	transact,
-    validateTransaction
-} from './payment.handler'
+import { transact, validateTransaction } from './payment.handler'
 
 export const paymentRouter = express.Router()
 paymentRouter.post('/', validateTransaction, transact)
