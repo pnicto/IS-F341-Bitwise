18397bce1efb9f75fcd47d8ec4f200b8d8aea8d5
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-19 03:26:47 +0530

    feat: notification utils for errors

---
+++ apps/frontend/src/notifications/utils.ts
@@ -0,0 +1,19 @@
+import { notifications } from '@mantine/notifications'
+import { AxiosError } from 'axios'
+
+export type APIErrors = { msg: string }[]
+
+export function notifyErrors(errors: APIErrors) {
+	errors.forEach((error) => {
+		notifications.show({
+			message: error.msg,
+			color: 'red',
+		})
+	})
+}
+
+export function handleAxiosErrors(error: unknown) {
+	if (error instanceof AxiosError) {
+		notifyErrors(error.response?.data.errors)
+	}
+}
