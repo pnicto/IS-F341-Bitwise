8ab4905a7d2b552478a7a6c9df0f2311a41e3176
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-19 05:19:53 +0530

    refactor: remove unnecessary elements for testing

--- apps/frontend/src/features/app.tsx
+++ apps/frontend/src/features/app.tsx
@@ -1,85 +1,10 @@
 // IMPORTANT: This file is a test file which might be removed in the future
-import { Button, List, Loader, PasswordInput, TextInput } from '@mantine/core'
-import { useForm } from '@mantine/form'
-import { User } from '@prisma/client'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import axios from 'axios'
-
-async function fetchUsers() {
-	const response = await axios.get<User[]>('http://localhost:5000/api/users')
-	return response.data
-}
 
 function App() {
-	const { isLoading, error, data, isPending } = useQuery({
-		queryKey: ['users'],
-		queryFn: () => fetchUsers(),
-	})
-	const queryClient = useQueryClient()
+	// probably this can be used to conditionally render role based options
+	// const data = useRouteLoaderData('protected-layout')
 
-	const createUser = useMutation({
-		mutationFn: (newUser: User) => {
-			return axios.post('http://localhost:5000/api/user', newUser)
-		},
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: ['users'] })
-		},
-	})
-
-	const form = useForm({
-		initialValues: { email: '', username: '', password: '' },
-		validate: {
-			username: (value) => (value.length > 0 ? null : 'Name cannot be empty'),
-			email: (value) => (value.length > 0 ? null : 'Invalid email'),
-			password: (value) => (value.length > 8 ? null : 'Password is too short'),
-		},
-	})
-
-	if (isLoading || isPending) return <Loader />
-
-	if (error) {
-		return <div>Error {error.message}</div>
-	}
-
-	return (
-		<main className='mx-auto max-w-xl p-24 text-center'>
-			<div>
-				<form
-					onSubmit={form.onSubmit((values) => {
-						// FIXME: remove typecast
-						createUser.mutate(values as User)
-					})}
-					className='flex flex-col gap-5'
-				>
-					<TextInput
-						label='Name'
-						description='Your name'
-						placeholder='Eg., John'
-						{...form.getInputProps('name')}
-					/>
-					<TextInput
-						label='Email'
-						description='Your email'
-						placeholder='Eg., john@john.com'
-						{...form.getInputProps('email')}
-					/>
-					<PasswordInput
-						label='Password'
-						description='Your password'
-						{...form.getInputProps('password')}
-					/>
-					<Button type='submit'>Create</Button>
-				</form>
-			</div>
-			<div className='m-10'>
-				<List>
-					{data.map((user, i) => (
-						<List.Item key={i}>{user.username}</List.Item>
-					))}
-				</List>
-			</div>
-		</main>
-	)
+	return <div>Main app</div>
 }
 
 export default App
