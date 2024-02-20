7180c431aad5467aa54084efc8abe7db82db9258
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-20 17:36:14 +0530

    refactor: use custom errors, status codes library, handle findUnique

--- apps/backend/src/features/auth/handler.ts
+++ apps/backend/src/features/auth/handler.ts
@@ -1,44 +1,52 @@
-import { Request, RequestHandler, Response } from 'express'
+import { RequestHandler } from 'express'
+import { StatusCodes } from 'http-status-codes'
 import { prisma } from '../../config/prisma'
 import { Unauthorized } from '../../errors/CustomErrors'
 import { generateAccessToken } from '../../utils/generateToken'
 import { verifyPassword } from './utils'
 
-export const login: RequestHandler = async (req: Request, res: Response) => {
-	const user = await prisma.user.findUniqueOrThrow({
-		where: { email: req.body.email },
-	})
+export const login: RequestHandler = async (req, res, next) => {
+	try {
+		const user = await prisma.user.findUnique({
+			where: { email: req.body.email },
+		})
 
-	const validPassword = await verifyPassword(req.body.password, user.password)
-	if (!validPassword) {
-		throw new Unauthorized('Invalid email or password')
+		if (!user) {
+			throw new Unauthorized('Invalid email or password')
+		}
+
+		const validPassword = await verifyPassword(req.body.password, user.password)
+		if (!validPassword) {
+			throw new Unauthorized('Invalid email or password')
+		}
+
+		const accessToken = generateAccessToken(user)
+
+		// TODO: Set correct redirect pathname
+		res
+			.status(StatusCodes.OK)
+			.cookie('jwt', accessToken, {
+				maxAge: 24 * 60 * 60 * 1000,
+				httpOnly: true,
+				domain: (process.env.FRONTEND_URL as string)
+					.replace('https://', '')
+					.replace('http://', '')
+					.split(':')[0],
+				secure: true,
+				sameSite: 'lax',
+			})
+			.json({
+				message: 'Logged in successfully',
+			})
+	} catch (err) {
+		next(err)
 	}
-
-	const accessToken = generateAccessToken(user)
-
-	// TODO: Set correct redirect pathname
-	res
-		.status(200)
-		.cookie('jwt', accessToken, {
-			maxAge: 24 * 60 * 60 * 1000,
-			httpOnly: true,
-			domain: (process.env.FRONTEND_URL as string)
-				.replace('https://', '')
-				.replace('http://', '')
-				.split(':')[0],
-			secure: true,
-			sameSite: 'lax',
-		})
-		.json({
-			message: 'Logged in successfully',
-			redirect: '/logout',
-		})
 }
 
-export const logout: RequestHandler = async (req: Request, res: Response) => {
+export const logout: RequestHandler = async (_req, res, next) => {
 	try {
 		res
-			.status(200)
+			.status(StatusCodes.OK)
 			.clearCookie('jwt', {
 				httpOnly: true,
 				domain: (process.env.FRONTEND_URL as string)
@@ -50,12 +58,10 @@ export const logout: RequestHandler = async (req: Request, res: Response) => {
 			})
 			.send({ message: 'Logged out successfully', redirect: '/login' })
 	} catch (err) {
-		res.status(500).send({
-			error: 'Something went wrong while logging out',
-		})
+		next(err)
 	}
 }
 
 export const check: RequestHandler = async (req, res) => {
-	res.status(200).json({ user: req.user, msg: 'Logged in' })
+	res.status(StatusCodes.OK).json({ user: req.user, msg: 'Logged in' })
 }
