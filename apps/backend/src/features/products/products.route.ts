7e7f5c8589acc77a35db308f045442de5daccc2d
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-02-20 22:02:08 +0530

    feat: use role authorization middleware for routes

--- apps/backend/src/features/products/products.route.ts
+++ apps/backend/src/features/products/products.route.ts
@@ -1,10 +1,22 @@
 import express from 'express'
+import passport from '../../config/passport'
+import { Role } from '@prisma/client'
+import { authorize } from '../../middleware/authorize'
 import {
 	createProduct,
 	getAllProducts,
 	validateNewProduct,
 } from './products.handler'
 
+const passportJWT = passport.authenticate('jwt', { session: false })
+
 export const productRouter = express.Router()
-productRouter.post('/new', validateNewProduct, createProduct)
+
+productRouter.post(
+	'/new',
+	passportJWT,
+	authorize(Role.VENDOR),
+	validateNewProduct,
+	createProduct,
+)
 productRouter.get('/', getAllProducts)
