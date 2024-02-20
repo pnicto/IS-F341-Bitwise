6c673e7b08b19d947d75c5795829615071b8707c
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-02-20 21:26:07 +0530

    feat: setup role authorization middleware

---
+++ apps/backend/src/middleware/authorize.ts
@@ -0,0 +1,20 @@
+import { Role } from '@prisma/client'
+import { NextFunction, Request, Response } from 'express'
+import { Forbidden } from '../errors/CustomErrors'
+import { getAuthorizedUser } from '../utils/getAuthorizedUser'
+
+export const authorize =
+	(...permittedRoles: Role[]) =>
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+            const user = getAuthorizedUser(req)
+
+			if (user.role && permittedRoles.includes(user.role)) {
+				next()
+			} else {
+                throw new Forbidden('You are not authorized to access this resource')
+			}
+		} catch (err) {
+			next(err)
+		}
+	}
