c114224974b1f34935e726c3aa7d917a08d49264
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-20 23:09:31 +0530

    refactor: fix typo in file, allow multiple roles as props

--- apps/frontend/src/shared/permission-guard.tsx
+++ apps/frontend/src/shared/permission-guard.tsx
@@ -1,24 +1,21 @@
 import { Role, User } from '@prisma/client'
 import { Outlet, useRouteLoaderData } from 'react-router-dom'
 
-type Props = { permission: Role }
+type Props = { permissions: Role[] }
 
 type RouteLoaderData = {
 	msg: string
 	user: Pick<User, 'id' | 'email' | 'role'>
 }
 
-const PermissionGuard = ({ permission }: Props) => {
+const PermissionGuard = ({ permissions }: Props) => {
 	const {
 		user: { role },
 	} = useRouteLoaderData('protected-layout') as RouteLoaderData
+	console.log(role)
 
-	return (
-		<>
-			<div>RoleGuard</div>
-			{role === permission ? <Outlet /> : <div>Stop snooping</div>}
-		</>
-	)
+	// TODO: Replace the div with something meaningful
+	return permissions.includes(role) ? <Outlet /> : <div>Stop snooping</div>
 }
 
 export default PermissionGuard
