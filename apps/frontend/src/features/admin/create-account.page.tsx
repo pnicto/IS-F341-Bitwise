90ae524fc69b54832da8725d8b0fc89590d5e816
Anurav-Garg <anurav.grg@gmail.com>
2024-02-21 22:28:10 +0530

    fix: remove input instead of hiding and make code more type strict

--- apps/frontend/src/features/admin/create-account.page.tsx
+++ apps/frontend/src/features/admin/create-account.page.tsx
@@ -1,13 +1,14 @@
 import { Button, Select, TextInput } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { notifications } from '@mantine/notifications'
+import { Role } from '@prisma/client'
 import { useMutation } from '@tanstack/react-query'
 import axios from '../../lib/axios'
 import { handleAxiosErrors } from '../../notifications/utils'
 
 const CreateAccount = () => {
-	const form = useForm({
-		initialValues: { email: '', role: 'Student', shopName: '' },
+	const form = useForm<{ email: string; role: Role; shopName: string }>({
+		initialValues: { email: '', role: Role.STUDENT, shopName: '' },
 		validate: {
 			email: (value) =>
 				value.length > 0
@@ -16,7 +17,7 @@ const CreateAccount = () => {
 						: 'Invalid email'
 					: 'Email cannot be empty',
 			shopName: (value, values) =>
-				values.role === 'Vendor'
+				values.role === Role.VENDOR
 					? value.length > 0
 						? null
 						: 'Shop name cannot be empty for vendor'
@@ -53,16 +54,20 @@ const CreateAccount = () => {
 			<Select
 				label='Role'
 				description='Role of the account to be created (Student/Vendor)'
-				data={['Student', 'Vendor']}
+				data={[
+					{ value: Role.STUDENT, label: 'Student' },
+					{ value: Role.VENDOR, label: 'Vendor' },
+				]}
 				{...form.getInputProps('role')}
 			/>
-			<TextInput
-				label='Shop Name'
-				description='Name of the shop owned by the vendor'
-				placeholder='Eg., YumPlease'
-				className={`${form.values.role === 'Student' ? 'hidden' : 'block'}`}
-				{...form.getInputProps('shopName')}
-			/>
+			{form.values.role === Role.VENDOR && (
+				<TextInput
+					label='Shop Name'
+					description='Name of the shop owned by the vendor'
+					placeholder='Eg., YumPlease'
+					{...form.getInputProps('shopName')}
+				/>
+			)}
 			<Button type='submit' loading={createAccount.isPending}>
 				Create
 			</Button>
