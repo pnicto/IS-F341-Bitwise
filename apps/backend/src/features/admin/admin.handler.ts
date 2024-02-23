7d78e2947c5f947ff17de2a72a2a7ae742bec575
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-23 18:11:52 +0530

    refactor: log newly created account password in dev

--- apps/backend/src/features/admin/admin.handler.ts
+++ apps/backend/src/features/admin/admin.handler.ts
@@ -54,9 +54,10 @@ export const createAccount: RequestHandler = async (req, res, next) => {
 		}
 
 		// send account creation emails only in production
-		if (process.env.NODE_ENV === 'production')
+		if (process.env.NODE_ENV === 'production') {
+			console.log(`DEV LOG: User: ${email}, Password: ${password}`)
 			await sendLoginCredentials(user, password)
-		else console.log('DEV LOG: Emails will only be sent in production')
+		} else console.log('DEV LOG: Emails will only be sent in production')
 
 		return res
 			.status(StatusCodes.CREATED)
