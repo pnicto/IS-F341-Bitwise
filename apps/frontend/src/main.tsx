0d77402ac9131f29b8344a2a754700b341e5708d
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-23 22:23:28 +0530

    feat(frontend): list available shops

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -15,6 +15,7 @@ import CreateAccount from './features/admin/create-account.page'
 import Login, { loginLoader } from './features/auth/login.page'
 import CreateProduct from './features/products/create-product.page'
 import ViewProducts from './features/products/view-products.page'
+import ShopList from './features/shops/shop-list.page'
 import HomeWithPayments from './features/user/home-with-payments.page'
 import ManageWallet from './features/user/manage-wallet.page'
 import ErrorBoundary from './shared/error-boundary'
@@ -47,6 +48,7 @@ const router = createBrowserRouter(
 					<Route index element={<HomeWithPayments />} />
 					<Route path='/products' element={<ViewProducts />} />
 					<Route path='/manage-wallet' element={<ManageWallet />} />
+					<Route path='/shops/view' element={<ShopList />} />
 				</Route>
 
 				{/* Protected for only vendor */}
