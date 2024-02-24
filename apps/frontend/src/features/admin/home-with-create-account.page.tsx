935536b6d4b3d8c183b38e59a244e9dcdabe8f99
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-02-24 15:33:07 +0530

    feat: add update account status functionality for admin

--- apps/frontend/src/features/admin/home-with-create-account.page.tsx
+++ apps/frontend/src/features/admin/home-with-create-account.page.tsx
@@ -1,13 +1,33 @@
 import { Button, Select, TextInput } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { notifications } from '@mantine/notifications'
-import { Role } from '@prisma/client'
-import { useMutation } from '@tanstack/react-query'
+import { Role, User } from '@prisma/client'
+import {
+	queryOptions,
+	useMutation,
+	useQuery,
+	useQueryClient,
+} from '@tanstack/react-query'
 import axios from '../../lib/axios'
 import { handleAxiosErrors } from '../../notifications/utils'
 
+const fetchUserDetails = async (email: string) => {
+	return axios
+		.get<{ user: User }>(`/admin/${email}/details`)
+		.then((res) => res.data)
+}
+
+const userDetailsQueryOptions = (email: string) =>
+	queryOptions({
+		queryKey: ['user', email],
+		queryFn: () => fetchUserDetails(email),
+		enabled: false,
+	})
+
 const HomeWithCreateAndUpdateAccount = () => {
-	const form = useForm<{ email: string; role: Role; shopName: string }>({
+	const queryClient = useQueryClient()
+
+	const createForm = useForm<{ email: string; role: Role; shopName: string }>({
 		initialValues: { email: '', role: Role.STUDENT, shopName: '' },
 		validate: {
 			email: (value) =>
@@ -25,12 +45,28 @@ const HomeWithCreateAndUpdateAccount = () => {
 		},
 	})
 
+	const getDetailsForm = useForm<{ email: string }>({
+		initialValues: { email: '' },
+		validate: {
+			email: (value) =>
+				value.length > 0
+					? /^\S+@\S+$/.test(value)
+						? null
+						: 'Invalid email'
+					: 'Email cannot be empty',
+		},
+	})
+
+	const userDetailsQueryResult = useQuery(
+		userDetailsQueryOptions(getDetailsForm.values.email),
+	)
+
 	const createAccount = useMutation({
 		mutationFn: (body: { email: string; role: Role }) => {
 			return axios.post<{ message: string }>('/admin/create', body)
 		},
 		onSuccess: ({ data }) => {
-			form.reset()
+			createForm.reset()
 			notifications.show({ message: data.message, color: 'green' })
 		},
 		onError: (error) => {
@@ -38,40 +74,110 @@ const HomeWithCreateAndUpdateAccount = () => {
 		},
 	})
 
+	const updateAccount = useMutation({
+		mutationFn: (body: { email: string; enabled: boolean }) => {
+			return axios.post<{ message: string }>('/admin/user/update-status', body)
+		},
+		onSuccess: ({ data }, variables) => {
+			queryClient.invalidateQueries({ queryKey: ['user', variables.email] })
+			notifications.show({ message: data.message, color: 'green' })
+			userDetailsQueryResult.refetch()
+		},
+		onError: (error) => {
+			handleAxiosErrors(error)
+		},
+	})
+
 	return (
-		<form
-			className='flex flex-col gap-5'
-			onSubmit={form.onSubmit((values) => {
-				createAccount.mutate(values)
-			})}
-		>
-			<TextInput
-				label='Email'
-				description='Email address of the account to be created'
-				placeholder='Eg., john@john.com'
-				{...form.getInputProps('email')}
-			/>
-			<Select
-				label='Role'
-				description='Role of the account to be created (Student/Vendor)'
-				data={[
-					{ value: Role.STUDENT, label: 'Student' },
-					{ value: Role.VENDOR, label: 'Vendor' },
-				]}
-				{...form.getInputProps('role')}
-			/>
-			{form.values.role === Role.VENDOR && (
+		<div className='flex flex-col gap-20'>
+			<form
+				className='flex flex-col gap-5'
+				onSubmit={createForm.onSubmit((values) => {
+					createAccount.mutate(values)
+				})}
+			>
 				<TextInput
-					label='Shop Name'
-					description='Name of the shop owned by the vendor'
-					placeholder='Eg., YumPlease'
-					{...form.getInputProps('shopName')}
+					label='Email'
+					description='Email address of the account to be created'
+					placeholder='Eg., john@john.com'
+					{...createForm.getInputProps('email')}
 				/>
+				<Select
+					label='Role'
+					description='Role of the account to be created (Student/Vendor)'
+					data={[
+						{ value: Role.STUDENT, label: 'Student' },
+						{ value: Role.VENDOR, label: 'Vendor' },
+					]}
+					{...createForm.getInputProps('role')}
+				/>
+				{createForm.values.role === Role.VENDOR && (
+					<TextInput
+						label='Shop Name'
+						description='Name of the shop owned by the vendor'
+						placeholder='Eg., YumPlease'
+						{...createForm.getInputProps('shopName')}
+					/>
+				)}
+				<Button type='submit' loading={createAccount.isPending}>
+					Create
+				</Button>
+			</form>
+			<form
+				className='flex flex-col gap-5'
+				onSubmit={getDetailsForm.onSubmit(() => {
+					userDetailsQueryResult.refetch()
+				})}
+			>
+				<TextInput
+					label='Email'
+					description='Email address of the account to be searched'
+					placeholder='Eg., john@john.com'
+					{...getDetailsForm.getInputProps('email')}
+				/>
+				<Button type='submit' loading={userDetailsQueryResult.isRefetching}>
+					Search User
+				</Button>
+			</form>
+			{userDetailsQueryResult.data !== undefined && (
+				<div className='flex flex-col border-2'>
+					<div className='text-center mt-2'>
+						<h1 className='text-2xl font-bold'>
+							{userDetailsQueryResult.data.user.email}
+						</h1>
+						<p className='text-xl'>
+							Account is{' '}
+							{userDetailsQueryResult.data.user.enabled
+								? 'Enabled'
+								: 'Disabled'}
+						</p>
+					</div>
+					{userDetailsQueryResult.data.user.enabled === false ? (
+						<Button
+							onClick={() => {
+								updateAccount.mutate({
+									email: userDetailsQueryResult.data.user.email,
+									enabled: true,
+								})
+							}}
+						>
+							Enable User
+						</Button>
+					) : (
+						<Button
+							onClick={() => {
+								updateAccount.mutate({
+									email: userDetailsQueryResult.data.user.email,
+									enabled: false,
+								})
+							}}
+						>
+							Disable User
+						</Button>
+					)}
+				</div>
 			)}
-			<Button type='submit' loading={createAccount.isPending}>
-				Create
-			</Button>
-		</form>
+		</div>
 	)
 }
 
