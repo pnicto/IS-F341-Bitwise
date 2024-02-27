0d315455683f6e4fe6bc8f50a39f2b370021110f
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-27 13:14:02 +0530

    refactor: move validation logic to validator, and reduce the number of nested conditionals in handler

--- apps/backend/src/features/users/user.handler.ts
+++ apps/backend/src/features/users/user.handler.ts
@@ -16,69 +16,59 @@ export const getUserDetails: RequestHandler = async (req, res) => {
 }
 
 export const validateNewDetails = [
-	body('mobile')
-		.isInt()
-		.optional({ checkFalsy: true })
-		.withMessage('The mobile number must be a number'),
-	body('oldPassword').optional({ checkFalsy: true }),
-	body('newPassword').optional({ checkFalsy: true }),
+	body('mobile').isMobilePhone('en-IN').optional(),
+	body('oldPassword').custom((value, { req }) => {
+		if (req.body.newPassword && !value) {
+			throw new Error('Old password is required when changing password')
+		} else if (value && !req.body.newPassword) {
+			throw new Error('New password is required when changing password')
+		} else if (value && value === req.body.newPassword) {
+			// the check on value is to ensure that when both are undefined this is not triggered
+			throw new Error('New password must be different from old password')
+		}
+		return true
+	}),
+	body('newPassword').optional(),
 ]
 export const editUserDetails: RequestHandler = async (req, res, next) => {
 	try {
 		const { mobile, oldPassword, newPassword } = validateRequest<{
-			mobile: string
-			oldPassword: string
-			newPassword: string
+			mobile: string | undefined
+			oldPassword: string | undefined
+			newPassword: string | undefined
 		}>(req)
+
 		const authorizedUser = getAuthorizedUser(req)
-		const user = await prisma.user.findUnique({
+
+		const userDetails = await prisma.user.findUnique({
+			where: { id: authorizedUser.id },
+			select: {
+				mobile: true,
+				password: true,
+			},
+		})
+
+		if (!userDetails) throw new NotFound('User not found')
+
+		if (mobile) {
+			userDetails.mobile = mobile
+		}
+
+		if (oldPassword && newPassword) {
+			const validPassword = await verifyPassword(
+				oldPassword,
+				userDetails.password,
+			)
+			if (!validPassword) throw new BadRequest('The old password is incorrect')
+			const newHashedPassword = await hashPassword(newPassword)
+			userDetails.password = newHashedPassword
+		}
+
+		await prisma.user.update({
+			data: { ...userDetails },
 			where: { id: authorizedUser.id },
 		})
 
-		if (!user) throw new NotFound('User not found')
-
-		if (
-			mobile === undefined &&
-			oldPassword === undefined &&
-			newPassword === undefined
-		) {
-			throw new BadRequest('All fields cannot be empty')
-		}
-		if (mobile !== undefined && mobile === user.mobile) {
-			throw new BadRequest('The mobile number is the same')
-		}
-		const newMobile = mobile === undefined ? user.mobile : mobile
-
-		if (oldPassword === undefined && newPassword !== undefined) {
-			throw new BadRequest('The old password must also be entered')
-		}
-
-		if (oldPassword !== undefined) {
-			if (newPassword === undefined) {
-				throw new BadRequest('The new password needs to be entered')
-			}
-			if (oldPassword === newPassword) {
-				throw new BadRequest(
-					'The new password must be different from the old password',
-				)
-			}
-
-			const validPassword = await verifyPassword(oldPassword, user.password)
-
-			if (!validPassword) {
-				throw new BadRequest('The old password is incorrect')
-			}
-		}
-
-		const newHashedPassword =
-			newPassword === undefined
-				? user.password
-				: await hashPassword(newPassword)
-
-		await prisma.user.update({
-			where: { username: user.username },
-			data: { mobile: newMobile, password: newHashedPassword },
-		})
 		return res.status(StatusCodes.OK).json({ message: 'Details updated' })
 	} catch (err) {
 		next(err)
