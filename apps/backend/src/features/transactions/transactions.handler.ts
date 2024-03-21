f37f7f531efa3dc7497cdaa7af0e83f3ba65221b
majimearun <arunachala0619@gmail.com>
2024-03-21 12:58:41 +0530

    feat: add viewTransactions functionality

---
+++ apps/backend/src/features/transactions/transactions.handler.ts
@@ -0,0 +1,88 @@
+import { RequestHandler } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { prisma } from '../../config/prisma'
+import { getAuthorizedUser } from '../../utils/getAuthorizedUser'
+
+export const viewTransactionHistory: RequestHandler = async (
+	req,
+	res,
+	next,
+) => {
+	try {
+		const sender = getAuthorizedUser(req)
+		const debitTransactions = await prisma.transaction.findMany({
+			where: {
+				senderUsername: sender.username,
+			},
+			select: {
+				amount: true,
+				senderUsername: true,
+				receiverUsername: true,
+				createdAt: true,
+			},
+		})
+		const creditTransactions = await prisma.transaction.findMany({
+			where: {
+				receiverUsername: sender.username,
+			},
+			select: {
+				amount: true,
+				senderUsername: true,
+				receiverUsername: true,
+				createdAt: true,
+			},
+		})
+		const walletTransactions = await prisma.walletTransactionHistory.findMany({
+			where: { userId: sender.id },
+			select: {
+				amount: true,
+				type: true,
+				createdAt: true,
+			},
+		})
+
+		const debitHistory = debitTransactions.map((transaction) => {
+			return {
+				amount: transaction.amount,
+				senderUsername: transaction.senderUsername,
+				receiverUsername: transaction.receiverUsername,
+				createdAt: transaction.createdAt,
+				type: 'DEBIT',
+			}
+		})
+
+		const creditHistory = creditTransactions.map((transaction) => {
+			return {
+				amount: transaction.amount,
+				senderUsername: transaction.senderUsername,
+				receiverUsername: transaction.receiverUsername,
+				createdAt: transaction.createdAt,
+				type: 'CREDIT',
+			}
+		})
+
+		const walletHistory = walletTransactions.map((transaction) => {
+			return {
+				amount: transaction.amount,
+				senderUsername: '-',
+				receiverUsername: '-',
+				createdAt: transaction.createdAt,
+				type: transaction.type,
+			}
+		})
+
+		const allTransactions = [
+			...debitHistory,
+			...creditHistory,
+			...walletHistory,
+		]
+
+		allTransactions.sort((a, b) => {
+			return b.createdAt.getTime() - a.createdAt.getTime()
+		})
+
+		return res.status(StatusCodes.OK).json(allTransactions)
+	} catch (err) {
+		next(err)
+	}
+}
