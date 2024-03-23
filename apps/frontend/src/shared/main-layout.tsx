341f135a52d4922a34db5b7ae51e64202efd5605
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-03-23 16:17:44 +0530

    feat: create new payment request

--- apps/frontend/src/shared/main-layout.tsx
+++ apps/frontend/src/shared/main-layout.tsx
@@ -1,5 +1,14 @@
 import { Icon } from '@iconify/react'
-import { Anchor, Button, Menu } from '@mantine/core'
+import {
+	Anchor,
+	Button,
+	Menu,
+	Modal,
+	NumberInput,
+	TextInput,
+} from '@mantine/core'
+import { useForm } from '@mantine/form'
+import { useDisclosure } from '@mantine/hooks'
 import { notifications } from '@mantine/notifications'
 import { User } from '@prisma/client'
 import { IconPlus } from '@tabler/icons-react'
@@ -16,6 +25,7 @@ import { handleAxiosErrors } from '../notifications/utils'
 
 const MainLayout = () => {
 	const data = useRouteLoaderData('protected-layout') as { user: User }
+	const [opened, { open, close }] = useDisclosure(false)
 
 	const currentRoute = useLocation()
 	const navigate = useNavigate()
@@ -32,6 +42,31 @@ const MainLayout = () => {
 		},
 		onError: (err) => {
 			handleAxiosErrors(err)
+		},
+	})
+
+	const paymentRequest = useMutation({
+		mutationFn: (body: { requesteeUsername: string; amount: number }) => {
+			return axios.post<{ message: string }>('/pay/request', body)
+		},
+		onSuccess: ({ data }) => {
+			notifications.show({ message: data.message, color: 'green' })
+			close()
+		},
+		onError: (err) => {
+			handleAxiosErrors(err)
+		},
+	})
+
+	const paymentRequestForm = useForm({
+		initialValues: {
+			requesteeUsername: '',
+			amount: 100,
+		},
+		validate: {
+			requesteeUsername: (value) =>
+				value.length > 0 ? null : 'Name cannot be empty',
+			amount: (value) => (value > 0 ? null : 'Please enter a valid amount.'),
 		},
 	})
 
@@ -62,6 +97,11 @@ const MainLayout = () => {
 								Bulk Add Users
 							</Anchor>
 						)}
+						{currentRoute.pathname === '/payment-requests' && (
+							<Button color='green' onClick={open}>
+								New Request
+							</Button>
+						)}
 
 						<Menu>
 							<Menu.Target>
@@ -87,6 +127,26 @@ const MainLayout = () => {
 				</nav>
 			)}
 			<main className='px-10 py-4 mx-auto sm:px-32 md:px-40 max-w-7xl'>
+				<Modal opened={opened} onClose={close} title='New Payment Request'>
+					<form
+						onSubmit={paymentRequestForm.onSubmit((values) =>
+							paymentRequest.mutate(values),
+						)}
+					>
+						<TextInput
+							label='Requestee Username'
+							placeholder='john43'
+							{...paymentRequestForm.getInputProps('requesteeUsername')}
+						/>
+						<NumberInput
+							label='Amount to request (INR)'
+							placeholder='40'
+							leftSection={<Icon icon='lucide:indian-rupee' />}
+							{...paymentRequestForm.getInputProps('amount')}
+						/>
+						<Button type='submit'>Request</Button>
+					</form>
+				</Modal>
 				<Outlet />
 			</main>
 		</>
