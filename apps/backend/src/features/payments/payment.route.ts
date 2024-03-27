538d521ba5b0a6242d282dcf5bd2bf3e21f0ab9c
majimearun <arunachala0619@gmail.com>
2024-03-27 11:54:42 +0530

    refactor: remove payment request handler functions from payment handler and route

--- apps/backend/src/features/payments/payment.route.ts
+++ apps/backend/src/features/payments/payment.route.ts
@@ -1,52 +1,13 @@
 import express from 'express'
 import { Role } from '@prisma/client'
 import { authorize } from '../../middleware/authorize'
-import {
-	transact,
-	validateTransaction,
-	requestPayment,
-	validatePaymentRequest,
-	respondToPaymentRequest,
-	validatePaymentRequestResponse,
-	validateGetPaymentRequests,
-	getPaymentRequests,
-	validateCancelPaymentRequest,
-	cancelPaymentRequest,
-} from './payment.handler'
+import { transact, validateTransaction } from './payment.handler'
 
 export const paymentRouter = express.Router()
-
-paymentRouter.get(
-	'/requests',
-	authorize(Role.STUDENT, Role.VENDOR),
-	validateGetPaymentRequests,
-	getPaymentRequests,
-)
 
 paymentRouter.post(
 	'/',
 	authorize(Role.STUDENT, Role.VENDOR),
 	validateTransaction,
 	transact,
-)
-
-paymentRouter.post(
-	'/request',
-	authorize(Role.STUDENT, Role.VENDOR),
-	validatePaymentRequest,
-	requestPayment,
-)
-
-paymentRouter.post(
-	'/respond',
-	authorize(Role.STUDENT, Role.VENDOR),
-	validatePaymentRequestResponse,
-	respondToPaymentRequest,
-)
-
-paymentRouter.post(
-	'/request/cancel',
-	authorize(Role.STUDENT, Role.VENDOR),
-	validateCancelPaymentRequest,
-	cancelPaymentRequest,
 )
