e4b10912a92cecc0a9a6fea3a65cb61e3da01d4c
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-04-06 12:00:49 +0530

    feat: improve the error page

--- apps/frontend/src/shared/loader.tsx
+++ apps/frontend/src/shared/loader.tsx
@@ -3,7 +3,7 @@ import { UseQueryResult } from '@tanstack/react-query'
 
 type Props<T> = {
 	children: (data: T) => React.ReactNode
-	errorComponent: React.ReactNode
+	errorComponent: string
 	query: UseQueryResult<T, unknown>
 }
 
@@ -12,7 +12,20 @@ const CustomLoader = <T,>({
 	errorComponent,
 	query,
 }: Props<T>) => {
-	if (query.isError) return errorComponent
+	if (query.isError)
+		return (
+			<div className='flex flex-col items-center justify-center text-center'>
+				<h1 className='text-2xl font-bold text-gray-800'>{errorComponent}</h1>
+				<p className='text-lg text-gray-600'>
+					We couldn't fetch the data you requested. Please try again later.
+				</p>
+				<img
+					src='/undraw_blank_canvas.svg'
+					alt='lost'
+					className='md:max-w-xl md:max-h-[30rem]'
+				/>
+			</div>
+		)
 
 	if (query.isPending)
 		return (
