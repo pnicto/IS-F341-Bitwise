002c42f8b2425ddf43ece54d2b0c18594bae61e7
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-04-16 19:33:20 +0530

    feat: get data in range intervals

--- apps/backend/src/features/reports/reports.handler.ts
+++ apps/backend/src/features/reports/reports.handler.ts
@@ -1,4 +1,5 @@
-import dayjs from 'dayjs'
+import { Transaction } from '@prisma/client'
+import dayjs, { ManipulateType } from 'dayjs'
 import { RequestHandler } from 'express'
 import { query } from 'express-validator'
 import { StatusCodes } from 'http-status-codes'
@@ -10,41 +11,107 @@ import { validateRequest } from '../../utils/validateRequest'
 export const validateVendorReport = [
 	query('preset')
 		.trim()
-		.isIn(['today', 'week', 'month', 'year'])
+		.isIn(['day', 'week', 'month', 'year'])
 		.optional()
 		.withMessage('Invalid preset'),
 	query('fromDate').trim().optional(),
 	query('toDate').trim().optional(),
 ]
+const getTimeIntervals = (
+	startDate: Date,
+	endDate: Date,
+	intervalUnit: ManipulateType,
+) => {
+	const intervals = []
+	let currentIntervalStart = dayjs(startDate)
+	const end = dayjs(endDate)
 
+	while (currentIntervalStart.isBefore(end)) {
+		const currentIntervalEnd = currentIntervalStart.clone().endOf(intervalUnit)
+		intervals.push([currentIntervalStart.toDate(), currentIntervalEnd.toDate()])
+		currentIntervalStart = currentIntervalStart.add(1, intervalUnit)
+	}
+
+	return intervals
+}
+const calculateDataForInterval = (
+	transactions: Transaction[],
+	interval: Date[],
+) => {
+	let label: string
+	const [startDate, endDate] = interval
+	const startDay = dayjs(startDate)
+	const endDay = dayjs(endDate)
+	const diffDays = endDay.diff(startDay, 'day') + 1
+	const diffHours = endDay.diff(startDay, 'hour') + 1
+
+	if (diffDays >= 28) {
+		label = startDay.format('MMMM')
+	} else if (diffDays >= 6) {
+		label = startDay.format('DD/MM')
+	} else if (diffDays >= 1 && diffHours > 1) {
+		label = startDay.format('dddd')
+	} else {
+		label = startDay.format('HH:mm')
+	}
+
+	const filteredTransactions = transactions.filter(
+		(transaction) =>
+			transaction.createdAt >= startDate && transaction.createdAt <= endDate,
+	)
+
+	const uniqueVisitors = new Set()
+	let totalIncome = 0
+
+	filteredTransactions.forEach((transaction) => {
+		uniqueVisitors.add(transaction.senderUsername)
+		totalIncome += transaction.amount
+	})
+
+	return {
+		uniqueVisitorsCount: uniqueVisitors.size,
+		totalIncome,
+		label,
+	}
+}
 const getStartAndEndDates = (preset: string) => {
 	const currentDate = dayjs()
 	switch (preset) {
-		case 'today':
+		case 'day': {
+			const startDate = currentDate.startOf('day').toDate()
+			const endDate = currentDate.endOf('day').toDate()
 			return {
-				startDate: currentDate.startOf('day').toDate(),
-				endDate: currentDate.endOf('day').toDate(),
+				startDate,
+				endDate,
 				compareStartDate: currentDate
 					.subtract(1, 'day')
 					.startOf('day')
 					.toDate(),
 				compareEndDate: currentDate.subtract(1, 'day').endOf('day').toDate(),
+				intervals: getTimeIntervals(startDate, endDate, 'hour'),
 			}
+		}
 
-		case 'week':
+		case 'week': {
+			const startDate = currentDate.startOf('week').toDate()
+			const endDate = currentDate.endOf('week').toDate()
 			return {
-				startDate: currentDate.startOf('week').toDate(),
-				endDate: currentDate.endOf('week').toDate(),
+				startDate,
+				endDate,
 				compareStartDate: currentDate
 					.subtract(1, 'week')
 					.startOf('week')
 					.toDate(),
 				compareEndDate: currentDate.subtract(1, 'week').endOf('week').toDate(),
+				intervals: getTimeIntervals(startDate, endDate, 'day'),
 			}
-		case 'month':
+		}
+		case 'month': {
+			const startDate = currentDate.startOf('month').toDate()
+			const endDate = currentDate.endOf('month').toDate()
 			return {
-				startDate: currentDate.startOf('month').toDate(),
-				endDate: currentDate.endOf('month').toDate(),
+				startDate,
+				endDate,
 				compareStartDate: currentDate
 					.subtract(1, 'month')
 					.startOf('month')
@@ -53,17 +120,23 @@ const getStartAndEndDates = (preset: string) => {
 					.subtract(1, 'month')
 					.endOf('month')
 					.toDate(),
+				intervals: getTimeIntervals(startDate, endDate, 'week'),
 			}
-		case 'year':
+		}
+		case 'year': {
+			const startDate = currentDate.startOf('year').toDate()
+			const endDate = currentDate.endOf('year').toDate()
 			return {
-				startDate: currentDate.startOf('year').toDate(),
-				endDate: currentDate.endOf('year').toDate(),
+				startDate,
+				endDate,
 				compareStartDate: currentDate
 					.subtract(1, 'year')
 					.startOf('year')
 					.toDate(),
 				compareEndDate: currentDate.subtract(1, 'year').endOf('year').toDate(),
+				intervals: getTimeIntervals(startDate, endDate, 'month'),
 			}
+		}
 		default:
 			throw new Error('Invalid preset')
 	}
@@ -84,23 +157,27 @@ export const getVendorReport: RequestHandler = async (req, res, next) => {
 		let startDate: Date,
 			endDate: Date,
 			compareStartDate: Date,
-			compareEndDate: Date
+			compareEndDate: Date,
+			intervals: Date[][]
+
 		if (preset) {
 			// https://github.com/prettier/prettier/issues/1935#issuecomment-306729468
 			// eslint-disable-next-line @typescript-eslint/no-extra-semi
-			;({ startDate, endDate, compareStartDate, compareEndDate } =
+			;({ startDate, endDate, compareStartDate, compareEndDate, intervals } =
 				getStartAndEndDates(preset))
 		} else if (fromDate && toDate) {
 			const fromDateObj = dayjs(fromDate)
 			const toDateObj = dayjs(toDate)
 			const diffInDays = toDateObj.diff(fromDateObj, 'day')
+
 			compareStartDate = fromDateObj.subtract(diffInDays, 'day').toDate()
 			compareEndDate = fromDateObj.subtract(1, 'day').toDate()
 			startDate = fromDateObj.toDate()
 			endDate = toDateObj.toDate()
+			intervals = getTimeIntervals(startDate, endDate, 'day')
 		} else {
 			// eslint-disable-next-line @typescript-eslint/no-extra-semi
-			;({ startDate, endDate, compareStartDate, compareEndDate } =
+			;({ startDate, endDate, compareStartDate, compareEndDate, intervals } =
 				getStartAndEndDates('month'))
 		}
 
@@ -113,6 +190,7 @@ export const getVendorReport: RequestHandler = async (req, res, next) => {
 				},
 			},
 		})
+
 		const compareTransactions = await prisma.transaction.findMany({
 			where: {
 				receiverUsername: vendor.shopName,
@@ -139,6 +217,10 @@ export const getVendorReport: RequestHandler = async (req, res, next) => {
 		})
 		const compareUniqueVisitorsCount = compareUniqueVisitors.size
 
+		const intervalData = intervals.map((interval) =>
+			calculateDataForInterval(currentTransactions, interval),
+		)
+
 		return res.status(StatusCodes.OK).json({
 			uniqueVisitorsCount: {
 				currentUniqueVisitorsCount,
@@ -148,6 +230,7 @@ export const getVendorReport: RequestHandler = async (req, res, next) => {
 				currentTotalIncome,
 				compareTotalIncome,
 			},
+			intervalData,
 		})
 	} catch (err) {
 		next(err)
